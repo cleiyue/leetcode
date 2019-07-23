da1dec9f7d2af646e495dace60d71996788f5a64
Aaro0n <mrcard97@gmail.com>
2019-07-23 18:36:55 +0800

    Dynamic Programming

--- 746.min-cost-climbing-stairs.c
+++ 746.min-cost-climbing-stairs.c
@@ -4,5 +4,13 @@
  * [746] Min Cost Climbing Stairs
  */
 
+#define MIN(a, b) (a > b ? b : a)
 int minCostClimbingStairs(int* cost, int costSize) {
+    int f1 = 0, f2 = 0;
+    for (int i = costSize - 1; i >= 0; --i) {
+        int f0 = cost[i] + MIN(f1, f2);
+        f2 = f1;
+        f1 = f0;
+    }
+    return MIN(f1, f2);
 }
