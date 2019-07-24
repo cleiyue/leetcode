d51eee8f57465eb66391ec53a9eb7fe49653ee43
Aaro0n <mrcard97@gmail.com>
2019-07-24 12:31:16 +0800

    Binary Prefix Divisible By 5

---
+++ 1018.binary-prefix-divisible-by-5.c
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1018 lang=c
+ *
+ * [1018] Binary Prefix Divisible By 5
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+bool* prefixesDivBy5(int* A, int ASize, int* returnSize) {
+    bool* ans = malloc(ASize * sizeof(bool));
+    *returnSize = ASize;
+    long long total = 0L;
+    for (int i = 0; i < ASize; i++) {
+        total = total << 1;
+        total += A[i];
+        if ((total = total % 5) == 0) {
+            ans[i] = true;
+        } else {
+            ans[i] = false;
+        }
+    }
+    return ans;
+}
