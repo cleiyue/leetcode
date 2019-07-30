5d6311861c95cddb3171565542ea5f56d857b212
Aaro0n <mrcard97@gmail.com>
2019-07-30 12:10:32 +0800

    Minimum Index Sum of Two Lists [simplify]

--- 599.minimum-index-sum-of-two-lists.c
+++ 599.minimum-index-sum-of-two-lists.c
@@ -9,27 +9,20 @@
  */
 char **findRestaurant(char **list1, int list1Size, char **list2, int list2Size, int *returnSize) {
     char **ans = malloc(list1Size * sizeof(char *));
-    int *index = malloc(list1Size * sizeof(int));
     int k = 2000;
     *returnSize = 0;
     for (int i = 0; i < list1Size; i++) {
         for (int j = 0; j < list2Size; j++) {
             if (strcmp(list1[i], list2[j]) == 0) {
-                ans[*returnSize] = list1[i];
-                index[*returnSize] = i + j;
-                (*returnSize)++;
                 if (i + j < k) {
                     k = i + j;
+                    *returnSize = 0;
+                    ans[(*returnSize)++] = list1[i];
+                } else if (i + j == k) {
+                    ans[(*returnSize)++] = list1[i];
                 }
             }
         }
     }
-    int t = 0;
-    for (int i = 0; i < *returnSize; i++) {
-        if (index[i] == k) {
-            ans[t++] = ans[i];
-        }
-    }
-    *returnSize = t;
     return ans;
 }
