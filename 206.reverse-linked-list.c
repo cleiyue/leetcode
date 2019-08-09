5ef2138c137a82405e3491545fbd4f472cce5090
Aaro0n <mrcard97@gmail.com>
2019-08-09 18:37:03 +0800

    Reverse Linked List [Iterative]

---
+++ 206.reverse-linked-list.c
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=206 lang=c
+ *
+ * [206] Reverse Linked List
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    while (curr != NULL) {
+        struct ListNode* nextTemp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
