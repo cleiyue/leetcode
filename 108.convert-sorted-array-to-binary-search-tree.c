3c04ad40bbd62ef0c067f6bfc96f2d1948efe600
Aaro0n <mrcard97@gmail.com>
2019-08-16 11:09:09 +0800

    Convert Sorted Array to Binary Search Tree

--- 108.convert-sorted-array-to-binary-search-tree.c
+++ 108.convert-sorted-array-to-binary-search-tree.c
@@ -13,16 +13,13 @@
  */
 
 struct TreeNode* convert(int* nums, int start, int end) {
-    if (start > end)
-        return NULL;
-    else {
-        int mid = (start + end) / 2;
-        struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
-        node->val = nums[mid];
-        node->left = convert(nums, start, mid - 1);
-        node->right = convert(nums, mid + 1, end);
-        return node;
-    }
+    if (start > end) return NULL;
+    int mid = (start + end) / 2;
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = nums[mid];
+    node->left = convert(nums, start, mid - 1);
+    node->right = convert(nums, mid + 1, end);
+    return node;
 }
 
 struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
