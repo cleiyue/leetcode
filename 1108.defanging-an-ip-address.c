93401944c4efd4ee9797ffa90f6fa1e61a938c8b
Aaro0n <mrcard97@gmail.com>
2019-08-21 10:00:56 +0800

    Defanging an IP Address

--- 1108.defanging-an-ip-address.c
+++ 1108.defanging-an-ip-address.c
@@ -6,17 +6,17 @@
 
 char* defangIPaddr(char* address) {
     int len = strlen(address);
-    char* ans = malloc((len + 7) * sizeof(char)); //+7,应为有\0
+    char* ans = malloc((len + 7) * sizeof(char));
     int k = 0;
     for (int i = 0; i < len; i++) {
         if (address[i] == '.') {
-            ans[k++] = '['; //用k++, 而不是 ans[k],k++
+            ans[k++] = '[';
             ans[k++] = '.';
             ans[k++] = ']';
         } else {
             ans[k++] = address[i];
         }
     }
-    ans[k] = '\0';  //一定要加，否则编译出错
+    ans[k] = '\0';
     return ans;
 }
