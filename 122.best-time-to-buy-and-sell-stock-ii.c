d8dc6badd196a44dcdfd31fd4691c15bd2bab6a7
Aaro0n <mrcard97@gmail.com>
2019-08-22 22:00:17 +0800

    Best Time to Buy and Sell Stock II

--- 122.best-time-to-buy-and-sell-stock-ii.c
+++ 122.best-time-to-buy-and-sell-stock-ii.c
@@ -6,17 +6,10 @@
 
 int maxProfit(int* prices, int pricesSize) {
     if (pricesSize < 2) return 0;
-    int valley = prices[0], peak = prices[0], maxprofit = 0, i = 0;
-    while (i < pricesSize - 1) {
-        while (i < pricesSize - 1 && prices[i] >= prices[i + 1]) {
-            i++;
-        }
-        valley = prices[i];
-        while (i < pricesSize - 1 && prices[i] <= prices[i + 1]) {
-            i++;
-        }
-        peak = prices[i];
-        maxprofit += peak - valley;
+    int maxprofit = 0;
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] > prices[i - 1])
+            maxprofit += prices[i] - prices[i - 1];
     }
     return maxprofit;
 }
