7b7732da5bf762ed571e3d1d9be418815b91a825
Aaro0n <mrcard97@gmail.com>
2019-08-22 16:46:21 +0800

    Longest Substring Without Repeating Characters [Sliding Window]

--- 3.longest-substring-without-repeating-characters.c
+++ 3.longest-substring-without-repeating-characters.c
@@ -4,10 +4,18 @@
  * [3] Longest Substring Without Repeating Characters
  */
 
-
-int lengthOfLongestSubstring(char * s){
-
+#define MAX(a, b) (a > b ? a : b)
+int lengthOfLongestSubstring(char *s) {
+    char *letter = calloc(127, sizeof(char));
+    int ln = strlen(s);
+    int ans = 0, i = 0, j = 0;
+    while (i < ln && j < ln) {
+        if (letter[s[j]] == 0) {
+            letter[s[j++]]++;
+            ans = MAX(ans, j - i);
+        } else {
+            letter[s[i++]]--;
+        }
+    }
+    return ans;
 }
-
-
-
