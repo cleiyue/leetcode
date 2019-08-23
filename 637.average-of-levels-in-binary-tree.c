0b768d85757218ca4553e681ed35288be380c95f
Aaro0n <mrcard97@gmail.com>
2019-08-23 15:30:21 +0800

    Average of Levels in Binary Tree [DFS]

--- 637.average-of-levels-in-binary-tree.c
+++ 637.average-of-levels-in-binary-tree.c
@@ -15,31 +15,26 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-void help(struct TreeNode* root, int level, int** ans, int* returnSize, int* returnColumnSizes) {
+void help(struct TreeNode* root, int level, double** ans, int* returnSize, int* returnColumnSizes) {
     if (root == NULL) return;
     if (level > *returnSize) {
-        ans[*returnSize] = malloc(1000 * sizeof(int));
+        ans[*returnSize] = calloc(1000, sizeof(double));
         (*returnSize)++;
     }
-    ans[level - 1][returnColumnSizes[level - 1]++] = root->val;
+    ans[level - 1][0] += root->val;
+    returnColumnSizes[level - 1]++;
     help(root->left, level + 1, ans, returnSize, returnColumnSizes);
     help(root->right, level + 1, ans, returnSize, returnColumnSizes);
 }
 
 double* averageOfLevels(struct TreeNode* root, int* returnSize) {
-    int** ans = malloc(1000 * sizeof(int*));
+    double** ans = malloc(1000 * sizeof(double*));
     *returnSize = 0;
     int* returnColumnSizes = calloc(1000, sizeof(int));
     help(root, 1, ans, returnSize, returnColumnSizes);
     double* dans = malloc(*returnSize * sizeof(double));
     for (int i = 0; i < *returnSize; i++) {
-        double total = 0;
-        int colSize = returnColumnSizes[i];
-        for (int j = 0; j < returnColumnSizes[i]; j++) {
-            total += ans[i][j];
-        }
-        if (colSize > 1) total = total / colSize;
-        dans[i] = total;
+        dans[i] = (double)ans[i][0] / returnColumnSizes[i];
     }
     return dans;
 }
