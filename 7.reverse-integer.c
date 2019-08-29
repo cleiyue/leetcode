a3e0e284b2f94eb0bbee7e210be380ac0d53c5f7
Aaro0n <mrcard97@gmail.com>
2019-08-29 11:27:28 +0800

    Reverse Integer

--- 7.reverse-integer.c
+++ 7.reverse-integer.c
@@ -4,17 +4,13 @@
  * [7] Reverse Integer
  */
 
-int reverse(int x) {
-    int max = 0x7fffffff, min = 0x80000000;
-    long temp = 0;
-    while (x != 0) {
-        temp = temp * 10 + x % 10;
+int reverse(int x){
+    long int ans = 0;
+    while(x){
+        ans *= 10;
+        ans += x%10;
+        if(ans > INT_MAX || ans < INT_MIN) return 0;
         x /= 10;
     }
-
-    if (temp > max || temp < min) {
-        return 0;
-    } else {
-        return (int)temp;
-    }
+    return ans;
 }
