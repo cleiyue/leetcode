9abbc11402dfb6ef81f1556c93fe852160b3349b
Aaro0n <mrcard97@gmail.com>
2019-09-01 16:17:12 +0800

    Next Permutation

--- 31.next-permutation.c
+++ 31.next-permutation.c
@@ -4,17 +4,33 @@
  * [31] Next Permutation
  */
 
-void nextPermutation(int *nums, int numsSize) {
-    // 如果是最大值，则返回最小值
-    for (int i = 1; i < numsSize; i++) {
-        if (nums[i] < nums[i - 1]) {
-        }
-    }
-
-    //
+void swap(int *arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
-//123 - 132 - 213 - 231 - 312 - 321
-//1234 -- 1243 -- 1324 --1342
+void reverse(int *arr, int n, int start) {
+    int i = start, j = n - 1;
+    while (i < j) {
+        swap(arr, i, j);
+        i++;
+        j--;
+    }
+}
 
-//321 - 123 - 132 - 213 - 231 - 312 - 321
+void nextPermutation(int *nums, int numsSize) {
+    int i = numsSize - 2;
+    while (i >= 0 && nums[i + 1] <= nums[i]) {
+        i--;
+    }
+    if (i >= 0) {
+        int j = numsSize - 1;
+        while (j > i && nums[j] <= nums[i]) {
+            j--;
+        }
+        swap(nums, i, j);
+    }
+    //递减序列翻转
+    reverse(nums, numsSize, i + 1);
+}
