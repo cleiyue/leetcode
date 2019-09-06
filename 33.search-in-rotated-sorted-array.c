cdeb9ccf611359f5992f73d927a3ebb64422501e
Aaro0n <mrcard97@gmail.com>
2019-09-06 11:01:44 +0800

    Search in Rotated Sorted Array

---
+++ 33.search-in-rotated-sorted-array.c
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=33 lang=c
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+int search(int* nums, int numsSize, int target) {
+    int low = 0, high = numsSize - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        if (nums[low] <= nums[mid]) {  //low==mid
+            if (target >= nums[low] && target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else {  //nums[low] > nums[mid]
+            if (target <= nums[high] && target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
