e330d24e71aa8aed91aadbf5d1e2ac14da9b8182
Aaro0n <mrcard97@gmail.com>
2019-09-28 21:55:15 +0800

    Two Sum

--- 1.two-sum.c
+++ 1.two-sum.c
@@ -4,13 +4,22 @@
  * [1] Two Sum
  */
 
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-
+int* twoSum(int* nums, int numsSize, int target) {
+    int* a = malloc(sizeof(int) * 2);
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = 0; j < numsSize; j++) {
+            if (i == j) {
+                continue;
+            }
+            if (nums[i] + nums[j] == target) {
+                a[0] = i;
+                a[1] = j;
+                return a;
+            }
+        }
+    }
+    return a;
 }
-
-
-
