bd8d3f4b6901c60a99b7f2d83c6b4c1267d96587
Aaro0n <mrcard97@gmail.com>
2019-10-22 16:18:34 +0800

    Palindromic Substrings

--- 647.palindromic-substrings.c
+++ 647.palindromic-substrings.c
@@ -5,4 +5,16 @@
  */
 
 int countSubstrings(char* s) {
+    int L = strlen(s);
+    int ans = 0;
+    for (int i = 0; i <= 2 * L - 1; ++i) {
+        int left = i / 2;
+        int right = left + i % 2;
+        while (left >= 0 && right < L && s[left] == s[right]) {
+            ans++;
+            left--;
+            right++;
+        }
+    }
+    return ans;
 }
