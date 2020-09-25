a1098582d25f0a1518987a1c283e0c95a1be8a5d
Aaro0n <mrcard97@gmail.com>
2020-09-25 19:41:50 +0800

    opt

--- string/67.add-binary.c
+++ string/67.add-binary.c
@@ -17,24 +17,27 @@ char* addBinary(char* a, char* b) {
     }
     char t = 0;
     for (int i = 0; i < size - 1; i++) {
-        if (ans[size - i - 2] != '0' && ans[size - i - 2] != '1') {
-            ans[size - i - 2] = '0';
+        int index = size - i - 2;
+        // memset(ans,'0',size) too slow
+        if (ans[index] != '0' && ans[index] != '1') {
+            ans[index] = '0';
         }
-        char r = ans[size - i - 2] + t + (w - i - 1 >= 0 ? b[w - i - 1] : '0');
-        t = 0;
-        if (r == '`') {  //'0'+'0'
-            ans[size - i - 2] = '0';
-        } else if (r == 'a') {  //'1'+'0'
-            ans[size - i - 2] = '1';
-        } else if (r == 'b') {  // 'b' = '1'+'1'
-            ans[size - i - 2] = '0';
+        char r = ans[index] + t + (w - i - 1 >= 0 ? b[w - i - 1] : '0');
+
+        if (r == '`') {
+            ans[index] = '0';
+            t = 0;
+        } else if (r == 'a') {
+            ans[index] = '1';
+            t = 0;
+        } else if (r == 'b') {
+            ans[index] = '0';
             t = 1;
-        } else {  // 'c' = '1'+'1'+'1'
-            ans[size - i - 2] = '1';
+        } else {
+            ans[index] = '1';
             t = 1;
         }
     }
-    if (ans[0] == '0') return ans + 1;
-    return ans;
+    return ans[0] == '0' ? ans + 1 : ans;
 }
 // @lc code=end
